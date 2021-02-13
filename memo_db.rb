d1d34745837e58ee60df666f78d50b0fbe4ade52
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-02-13 10:32:22 +0900

    feat: server起動時にdbとtableを生成しないようにした
    作成する場合は `ruby setup.rb` を実行する

--- memo_db.rb
+++ memo_db.rb
@@ -5,12 +5,39 @@ require 'erb'
 
 class MemoDB
   def initialize
-    create_db unless exist_db?
-    create_table
     sync_memos
   end
 
   attr_reader :memos
+
+  class << self
+    def setup
+      create_db unless exist_db?
+      create_table
+    end
+
+    def exist_db?
+      `psql -U postgres -l | grep memo_with_sinatra` != ''
+    end
+
+    def create_db
+      `psql -U postgres -c '
+        CREATE DATABASE memo_with_sinatra WITH OWNER postgres ENCODING = "UTF8"
+      '`
+    end
+
+    def create_table
+      connect.exec('
+        CREATE TABLE IF NOT EXISTS memos
+        ( id serial PRIMARY KEY, title text not null, body text not null)
+      ')
+    end
+
+    def connect
+      PG.connect(user: 'postgres', dbname: 'memo_with_sinatra')
+    end
+  end
+
 
   def add(title, body)
     execute_sql(
@@ -36,33 +63,8 @@ class MemoDB
 
   private
 
-  def html_escape(str)
-    ERB::Util.html_escape(str)
-  end
-
-  def connect
-    PG.connect(user: 'postgres', dbname: 'memo_with_sinatra')
-  end
-
-  def exist_db?
-    `psql -U postgres -l | grep memo_with_sinatra` != ''
-  end
-
-  def create_db
-    `psql -U postgres -c '
-      CREATE DATABASE memo_with_sinatra WITH OWNER postgres ENCODING = "UTF8"
-    '`
-  end
-
-  def create_table
-    connect.exec('
-      CREATE TABLE IF NOT EXISTS memos
-      (id serial PRIMARY KEY, title text not null, body text not null)
-    ')
-  end
-
   def execute_sql(stmt_name, sql, params)
-    connection = connect
+    connection = MemoDB.connect
     connection.prepare(stmt_name, sql)
     connection.exec_prepared(stmt_name, params)
     connection.finish
@@ -70,7 +72,7 @@ class MemoDB
   end
 
   def sync_memos
-    connection = connect
+    connection = MemoDB.connect
     @memos = connection.exec('SELECT * FROM memos ORDER BY id ASC') do |records|
       records.to_h do |record|
         [record['id'].to_sym, { title: record['title'], body: record['body'] }]
